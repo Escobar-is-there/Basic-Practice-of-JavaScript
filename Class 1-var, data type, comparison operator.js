7c0181c2f17164e8d86de7fc963e7e8c31c8478c
Escobar-is-there <157167545+Escobar-is-there@users.noreply.github.com>
2024-02-28 03:40:33 +0600

    Uploaded First file

---
+++ Class 1-var, data type, comparison operator.js
@@ -0,0 +1,28 @@
+//Question 1
+var firstName = "Rafiqul Alam"
+var lastName = "Imtiaj"
+var Fullname = firstName + " " + lastName
+console.log(Fullname)
+//Question 2
+var a = 5
+var b = "60"
+console.log(a + Number(b))
+//Question 3
+var Num1 = 59
+var txt1 = "hello JS"
+console.log("The type of Num1 is:" ,typeof(Num1))
+console.log("The type of txt1 is:" ,typeof(txt1))
+console.log(Num1 !== txt1)
+//Question 4
+var Num2 = 344
+var txtNum = "344"
+console.log("The type of Num1 is:" ,typeof(Num2))
+console.log("The type of txt1 is:" ,typeof(txtNum))
+console.log(Num2 == txtNum)
+console.log(Num2 === txtNum)
+console.log(Num2 !== txtNum)
+//Question 5
+var oddNum = 75;
+var evenNum = 90;
+var boolValue = oddNum !== evenNum;
+console.log(boolValue);
