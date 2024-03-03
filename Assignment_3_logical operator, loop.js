0652c7595fe25791e071e5adc803f4b1a99557b3
Rafiqul Alam Imtiaj <157167545+Escobar-is-there@users.noreply.github.com>
2024-03-03 17:05:56 +0600

    Update Assignment_3_logical operator, loop.js

--- Assignment_3_logical operator, loop.js
+++ Assignment_3_logical operator, loop.js
@@ -1,85 +1,63 @@
 //Question 1
-// var prompt = require('prompt-sync')();
-// var firstnumber = Number(prompt("Enter The First Number of Your Series "))
-// var lastnumber = Number(prompt("Enter The Last Number of Your Series "))
-// var i = firstnumber
-// var sum = 0
-// for (let i = firstnumber; i <=lastnumber; i++) {
-//     sum = sum + i;
-//     console.log(i);
-// }
-// console.log("The Sum of The Series : ",sum )
+
+/* 1. Write code in JS: 
+Write a code for javascript that takes two numbers, the first and last numbers of a series, as input from the user.
+The program should then calculate and display the sum of all the numbers in that series. 
+For example: If the user inputs 3 and 8, the program should calculate and print the sum: 3 + 4 + 5 + 6 + 7 + 8. 
+If the user inputs 1 and 10, the program should calculate and print the sum: 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10.
+*/
+var prompt = require('prompt-sync')();
+var firstnumber = Number(prompt("Enter The First Number of Your Series "))
+var lastnumber = Number(prompt("Enter The Last Number of Your Series "))
+var i = firstnumber
+var sum = 0
+for (let i = firstnumber; i <=lastnumber; i++) {
+    sum = sum + i;
+    console.log(i);
+}
+console.log("The Sum of The Series : ",sum )
 
 //Question 2
-// var sum = 0
-// for (var i = 1; i <= 500; i++) {
-//         if (i%2===0) {
-//             console.log(i) 
-//             sum = sum + i; 
-//     }  
-// }
-// console.log("The Sum of all even number Between 1 and 500 is : ",sum )
+
+/* 2. Write code in JS : Write a code to calculate and display the sum of all even numbers between 1 and 500.
+*/
+var sum = 0
+for (var i = 1; i <= 500; i++) {
+        if (i%2===0) {
+            console.log(i) 
+            sum = sum + i; 
+    }  
+}
+console.log("The Sum of all even number Between 1 and 500 is : ",sum )
 
 
 //Question 3
-// var sum = 0
-// for (var i = 1; i <= 500; i++) {
-//         if (i%2!==0) {
-//             console.log(i) 
-//             sum = sum + i; 
-//     }  
-// }
-// console.log("The Sum of all Odd number Between 1 and 500 is : ",sum )
+
+/* 3. Write code in JS : Write a code to calculate and display the sum of all odd numbers between 1 and 500.
+*/
+var sum = 0
+for (var i = 1; i <= 500; i++) {
+        if (i%2!==0) {
+            console.log(i) 
+            sum = sum + i; 
+    }  
+}
+console.log("The Sum of all Odd number Between 1 and 500 is : ",sum )
 
 //Question 4
-// var a = 3
-// var d = 5
-// var n = 100
-// var nth_term = a+(n-1)*d
-// console.log("The n_th Term of series : ",nth_term)
-// var sum = n/2*(2*a+(n-1)*d)
-// console.log("The Sum of The Series up to 100th Term is : ",sum)
 
+/* 4. Run the below code in JS, write your output:
+Suppose we have a series: 3+8+13+........  
 
+- Find the 100th term of this series. The code will display the value of the 100th term of this series. You can use the formula: nth_term = a + (n-1) d 
 
+- Display the sum of the series up to the 100th term.
+*/
+var a = 3
+var d = 5
+var n = 100
+var nth_term = a+(n-1)*d
+console.log("The n_th Term of series : ",nth_term)
+var sum = n/2*(2*a+(n-1)*d)
+console.log("The Sum of The Series up to 100th Term is : ",sum)
 
-
-// var name = prompt("Enter Your Name : ")
-// var age = prompt("Enter Your Age : ")
-
-// if (name == "Shelby" && "Family" && age<30) {
-//   console.log("Welcome To : ",name)  
-// } else {
-//     console.log("Sorry! You Are Not Authorized ")
-// }
-// var num = Number(prompt("Enter Your Marks : "))
-// if (num<39) {
-//     console.log("Your Grade is F")
-// }
-// else if (num>=40 && num<=44) {
-//     console.log("Your Grade is D")   
-// }
-// else if (num>=45 && num<=49) {
-//     console.log("Your Grade is C")   
-// }
-// else if (num>=50 && num<=54) {
-//     console.log("Your Grade is C+")   
-// }
-// else if (num>=55 && num<=59) {
-//     console.log("Your Grade is B-")   
-// }
-// else if (num>=60 && num<64) {
-//     console.log("Your Grade is B")   
-// }
-// else if (num>=65 && num<=69) {
-//     console.log("Your Grade is B+")   
-// }
-// else if (num>=70 && num<=74) {
-//     console.log("Your Grade is A-")   
-// }
-// else if (num>=75 && num<=79) {
-//     console.log("Your Grade is A")   
-// }
-// else if (num>=80 && num<=100) {
-//     console.log("Great!!!,Your Grade is A+")   
-// }
